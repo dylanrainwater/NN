749b7831e0cff20bb8e00b74193e17b3d72de211
Dylan Rainwater <drainwa1@vols.utk.edu>
2018-01-31 17:04:58 -0500

    Finish MVP of neural network implementation

--- NN.cpp
+++ NN.cpp
@@ -1,16 +1,96 @@
 #include "NN.h"
+#include <fstream>
 
-NN::NN(std::string input_file, int label_col=-1)
+NN::NN(std::string input_file)
 {
     this->input_file = input_file;
     this->data = std::vector <std::vector <double>>();
     this->predictions = std::vector <std::string>();
+    this->alpha = 0.01;
+    
+    populateData();
 
-    //TODO Read input file and populate data
+    /* Populate weights and bias term */
+    srand(time(NULL));
+    for (int i = 0; i < data[0].size(); i++)
+        weights.push_back(randomDouble());
+    bias = randomDouble();
+}
+
+void NN::setAlpha(double alpha)
+{
+    this->alpha = alpha;
+}
+
+double NN::randomDouble()
+{
+    return (double) rand()*2 / (double) RAND_MAX - 1;
+}
+
+void NN::populateData()
+{
+    std::ifstream data_file;
+    data_file.open(input_file);
+    if (!data_file)
+        perror(input_file.c_str());
+
+    std::string line;
+
+    while (getline(data_file, line)) {
+        std::stringstream parser(line);
+
+        double x;
+        std::string y;
+        std::vector <double> data_point;
+
+        while (parser >> y) {
+            if (atof(y.c_str())) {
+                x = atof(y.c_str());
+                data_point.push_back(x);
+            } else { 
+                known_labels.push_back(y);
+            }
+        }
+
+        data.push_back(data_point);
+    }
 }
 
 void NN::train_test_split()
 {
+}
+
+double NN::getPerfectVal(Label& label)
+{
+    return getPerfectVal(label.label);
+}
+
+double NN::getPerfectVal(std::string& label_name)
+{
+    double perfect_val = -1;
+    for (int i = 0; i < labels.size(); i++)
+        if (labels[i].label == label_name)
+            perfect_val = labels[i].perfect_value;
+    return perfect_val;
+}
+
+void NN::createLabel(double perfect_value, double lower_bound, double upper_bound, std::string& label_name) 
+{
+    Label label;
+    label.lower_bound = lower_bound;
+    label.upper_bound = upper_bound;
+    label.perfect_value = perfect_value;
+    label.label = label_name;
+    labels.push_back(label);
+}
+
+std::string NN::getLabel(double prediction)
+{
+    std::string pred_label = "";
+    for (Label l : labels)
+        if (prediction >= l.lower_bound && prediction <= l.upper_bound)
+            pred_label = l.label;
+    return pred_label;
 }
 
 void NN::addData(std::vector <std::vector <double>>& data)
@@ -24,20 +104,80 @@ void NN::addDataPoint(std::vector <double>& data_point)
     this->data.push_back(data_point);
 }
 
-void NN::train(std::string file_name="", int iterations=1000)
+/* Train neural network on data by using backpropagation with variable
+ * iterations */
+void NN::train(int iterations)
 {
+    std::vector <double> costs;
+    std::cout << "Before training, b = " << bias << " w = {";
+    for (int i = 0; i < weights.size(); i++)
+        std::cout << weights[i] << ", ";
+    std::cout << "}" << std::endl;
 
+    for (int i = 0; i < iterations; i++) {
+        int random_index = rand() % data.size();
+        std::vector <double> data_point = data[random_index];
+
+		double z = 0;
+        for (int j = 0; j < data_point.size(); j++)
+            z += data_point[j] * weights[j];
+        z += bias;
+
+        double prediction = sigmoid(z);
+        double target = getPerfectVal(known_labels[random_index]);
+
+        double pred_cost = cost(prediction, target);
+        costs.push_back(pred_cost);
+
+        /* Each variable of the name "da_db" is the derivative of a
+         * with respect to b. This is to perform gradient descent in
+         * order to adjust our weights and bias terms. This is the
+         * part where we are actually training our network. */
+        double dcost_dpred, dpred_dz, dz_db, dcost_dz,
+               dcost_dw1, dcost_dw2, dcost_db;
+        std::vector <double> dz_dweight, dcost_dweight;
+
+        dcost_dpred = 2 * (prediction - target);
+        dpred_dz = sigmoid_derivative(z);
+
+        for (int j = 0; j < weights.size(); j++)
+            dz_dweight.push_back(data_point[j]);
+        dz_db = 1;
+
+        dcost_dz = dcost_dpred * dpred_dz;
+        for (int j = 0; j < weights.size(); j++)
+            dcost_dweight.push_back(dcost_dz * dz_dweight[j]);
+
+        dcost_db = dcost_dz * dz_db;
+    
+        /* Adjust weights and bias */
+        for (int j = 0; j < weights.size(); j++)
+            weights[j] -= alpha * dcost_dweight[j];
+
+        bias -= alpha * dcost_db;
+    }
+    std::cout << "Costs " << costs[0] << " --> " << costs[costs.size() - 1] << std::endl;
 }
 
-std::vector <std::string> NN::predict(std::vector <double>& data)
+std::vector <double> NN::predict(std::vector <std::vector <double>>& data)
 {
-    std::vector <std::string> predictions;
+    std::vector <double> predictions;
+
+    for (std::vector <double>& data_point : data)
+        predictions.push_back(predict(data_point));
+
     return predictions;
 }
 
-std::string NN::predict(double data_point)
+double NN::predict(std::vector <double>& data_point)
 {
-    std::string prediction;
+    double prediction = 0.0;
+
+    for (int i = 0; i < data_point.size(); i++)
+        prediction += data_point[i] * weights[i];
+
+    prediction += bias;
+    prediction = sigmoid(prediction);
     return prediction;
 }
 
@@ -65,4 +205,16 @@ double NN::cost(std::vector <double>& targets, std::vector <double>& predictions
 double NN::cost(double target, double prediction)
 {
     return (prediction - target) * (prediction - target);
+}
+
+double NN::test(int num) 
+{
+    if (num > data.size()) num = data.size();
+    double accuracy = 0;
+    for (int i = 0; i < num; i++) {
+        if (getLabel(predict(data[i])) == known_labels[i])
+            accuracy++;
+    }
+    accuracy /= num;
+    return accuracy;
 }
--- NN.h
+++ NN.h
@@ -2,29 +2,50 @@
 #define D_Neural_Network
 
 #include <string>
+#include <sstream>
 #include <vector>
 #include <cmath>
+#include <iostream>
+
+struct Label {
+    std::string label;
+    double lower_bound;
+    double upper_bound;
+    double perfect_value;
+};
 
 class NN {
     private:
         std::string input_file;
         std::vector <std::vector <double>> data;
+        std::vector <std::string> known_labels;
         std::vector <std::string> predictions;
         std::vector <double> weights;
+        std::vector <Label> labels;
+        double alpha;
         double bias;
+        double randomDouble();
     public:
-        NN(std::string input_file, int label_col);
+        NN(std::string input_file);
+
+        double getPerfectVal(Label& label);
+        double getPerfectVal(std::string& label_name);
+        void populateData();
         void train_test_split();
+        void createLabel(double perfect, double lower, double upper, std::string& name);
+        std::string getLabel(double prediction);
         void addData(std::vector <std::vector <double>>& data); /* To set data without the need of a file */
         void addDataPoint(std::vector <double>& data_point);
-        void train(std::string file_name, int iterations); /* Train neural network on data */
-        std::vector <std::string> predict(std::vector <double>& data);
-        std::string predict(double data_point);
+        void train(int iterations=1000); /* Train neural network on data */
+        std::vector <double> predict(std::vector <std::vector <double>>& data);
+        double predict(std::vector <double>& data_point);
         double sigmoid(double n);
         double sigmoid_derivative(double n);
         double cost(std::vector <double>& targets, std::vector <double>& predictions);
         double cost(double target, double prediction);
-        /* TODO: Add hyperparameter tuning options */
+        double reset(bool keep_weights);
+        double test(int num);
+        void setAlpha(double alpha);
 };
 
 #endif
