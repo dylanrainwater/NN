8b3d1b9f3b8742ce99086043eada49de30b5e504
Dylan Rainwater <dylanrainwater@users.noreply.github.com>
2018-02-13 15:28:55 -0500

    Add some clarifying comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
  * neural network library.
  */
 int main(int argc, char **argv) {
+    /* Our example program will take in a filename and a variable number of labels
+       to classify data on. */
     if (argc <= 3) {
         cerr << "Usage: NN filename label1 label2 [label3] ..." << endl;
         return -1;
@@ -16,11 +18,13 @@ int main(int argc, char **argv) {
 
     NN classifier(file_name);
 
+    // Just get our list of labels from the command line
     int num_labels = argc - 2;
     vector <string> labels;
     for (int i = 2; i < argc; i++)
         labels.push_back(string(argv[i]));
 
+    // Generate our ranges for labels (i.e. label 1 is [0, 0.5), label 2 is [0.5, 1]).
     int j = 0;
     for (double i = 0; i < 1; i += (1.0 / num_labels), j++) {
         double perfect_value, lower_bound, upper_bound;
@@ -34,6 +38,7 @@ int main(int argc, char **argv) {
         classifier.createLabel(perfect_value, lower_bound, upper_bound, labels[j]);
     }
 
+    // Train the classifier for n=100,000 intervals.
     classifier.train(100000);
 
     int num = 100;
